ba7e21b026a2b631fb967ea6fad31cf4ad0eb33c
Yoann Abbes <yoann.abbes@gmail.com>
2019-02-05 16:15:13 +0100

    script requestMId use mCreate

--- gatling/user-files/utils/requestMIds.js
+++ gatling/user-files/utils/requestMIds.js
@@ -2,24 +2,45 @@ const rp = require('request-promise');
 const fs = require('fs');
 let i = parseInt(process.argv[2]) || 1;
 
+const doc = 
+{
+  "body":
+    {
+      "driver": {
+        "name": "Eltooooon",
+        "age": 42,
+        "license": "B"
+      },
+
+      "car": {
+        "position": {
+          "lat": 42.83734827,
+          "lng": 8.298382039
+        },
+        "type": "berline"
+      }
+    }
+}; 
+const docs = [];  
+for (let it_doc = 0; it_doc < 200; it_doc++)
+    docs.push(doc);
+const content = JSON.parse('{ \"documents\": ' + JSON.stringify(docs) + '}');
 const options = {
     method: 'POST',
-    uri: 'http://localhost:7512/nyc-open-data/yellow-taxi/_create',
-    body: {
-        "user": "yo", 
-        "pwd":"nodenodenode"
-    },
+    uri: 'http://localhost:7512/nyc-open-data/yellow-taxi/_mCreate',
+    body: content,
     json: true
 };
- 
+
 const tab_ids = [];
 const retrieve_ids = async () => {
     for (; i > 0; i--) {
         try {
-            const parsedBody = await rp(options)            
-            tab_ids.push(parsedBody.result._id)
+            const parsedBody = await rp(options)
+            for (let it = 0; parsedBody.result.hits[it]; ++it)            
+                tab_ids.push(parsedBody.result.hits[it]._id)
         } catch (error) {
-            console.log(error);
+            console.log("nop");
         }
     }
     fs.writeFileSync('./ids.txt', JSON.stringify(tab_ids).replace(/,/g, ', '));
