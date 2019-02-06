eb6a0f9390027ca71538a4e74c86785b9db6655a
Yoann Abbes <yoann.abbes@gmail.com>
2019-02-06 14:14:14 +0100

    js coding style

--- gatling/user-files/utils/requestMIds.js
+++ gatling/user-files/utils/requestMIds.js
@@ -1,13 +1,13 @@
-const rp = require("request-promise");
-const fs = require("fs");
+const rp = require('request-promise');
+const fs = require('fs');
 let count = parseInt(process.argv[2]) || 1;
 
 const doc = {
   body: {
     driver: {
-      name: "Eltooooon",
+      name: 'Eltooooon',
       age: 42,
-      license: "B"
+      license: 'B'
     },
 
     car: {
@@ -15,16 +15,16 @@ const doc = {
         lat: 42.83734827,
         lng: 8.298382039
       },
-      type: "berline"
+      type: 'berline'
     }
   }
 };
 const docs = [];
-for (let it = 0; it < 200; it++) docs.push(doc);
-const content = JSON.parse('{ "documents": ' + JSON.stringify(docs) + "}");
+for (let it = 0; it < 200; it++) {docs.push(doc);}
+const content = JSON.parse('{ "documents": ' + JSON.stringify(docs) + '}');
 const options = {
-  method: "POST",
-  uri: "http://localhost:7512/nyc-open-data/yellow-taxi/_mCreate",
+  method: 'POST',
+  uri: 'http://localhost:7512/nyc-open-data/yellow-taxi/_mCreate',
   body: content,
   json: true
 };
@@ -35,11 +35,11 @@ const retrieve_ids = async () => {
     try {
       const parsedBody = await rp(options);
       for (let it = 0; parsedBody.result.hits[it]; ++it)
-        tab_ids.push(parsedBody.result.hits[it]._id);
+      {tab_ids.push(parsedBody.result.hits[it]._id);}
     } catch (error) {
       console.error(error);
     }
   }
-  fs.writeFileSync("./ids.txt", JSON.stringify(tab_ids).replace(/,/g, ", "));
+  fs.writeFileSync('./ids.txt', JSON.stringify(tab_ids).replace(/,/g, ', '));
 };
 retrieve_ids();
--- gatling/user-files/utils/requestOneId.js
+++ gatling/user-files/utils/requestOneId.js
@@ -1,12 +1,12 @@
-const rp = require("request-promise");
-const fs = require("fs");
+const rp = require('request-promise');
+const fs = require('fs');
 
 const options = {
-  method: "POST",
-  uri: "http://localhost:7512/nyc-open-data/yellow-taxi/_create",
+  method: 'POST',
+  uri: 'http://localhost:7512/nyc-open-data/yellow-taxi/_create',
   body: {
-    user: "yo",
-    pwd: "nodenodenode"
+    user: 'yo',
+    pwd: 'nodenodenode'
   },
   json: true
 };
@@ -14,7 +14,7 @@ const options = {
 const retrieve_id = async () => {
   try {
     const parsedBody = await rp(options);
-    fs.writeFileSync("./id.txt", parsedBody.result._id);
+    fs.writeFileSync('./id.txt', parsedBody.result._id);
   } catch (err) {
     console.error(err);
   }
