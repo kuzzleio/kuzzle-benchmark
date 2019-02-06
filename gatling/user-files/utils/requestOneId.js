76b586d35a9208df9b14614cb60724b284a2c427
Yoann Abbes <yoann.abbes@gmail.com>
2019-02-06 10:06:35 +0100

    indent 2 spaces

--- gatling/user-files/utils/requestOneId.js
+++ gatling/user-files/utils/requestOneId.js
@@ -2,22 +2,21 @@ const rp = require('request-promise');
 const fs = require('fs');
 
 const options = {
-    method: 'POST',
-    uri: 'http://localhost:7512/nyc-open-data/yellow-taxi/_create',
-    body: {
-        "user": "yo", 
-        "pwd":"nodenodenode"
-    },
-    json: true
+  method: 'POST',
+  uri: 'http://localhost:7512/nyc-open-data/yellow-taxi/_create',
+  body: {
+    "user": "yo",
+    "pwd": "nodenodenode"
+  },
+  json: true
 };
 
-
 const retrieve_id = async () => {
-    try {
-        const parsedBody = await rp(options)
-        fs.writeFileSync('./id.txt', parsedBody.result._id)
-    } catch (error) {
-        console.log(error);            
-    }
+  try {
+    const parsedBody = await rp(options)
+    fs.writeFileSync('./id.txt', parsedBody.result._id)
+  } catch (err) {
+    console.error(err);
+  }
 }
 retrieve_id();
