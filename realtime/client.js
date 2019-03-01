0a4ed479ae3a50b047b5c1aa3b60f483d55242c9
Aschen <amaret93@gmail.com>
2019-03-01 08:19:27 +0100

    Add realtime clients

---
+++ realtime/client.js
@@ -0,0 +1,90 @@
+const
+  fs = require('fs'),
+  {
+    Kuzzle,
+    WebSocket
+  } = require('kuzzle-sdk');
+
+class Client {
+  constructor (id, host, port, expectedNotifications) {
+    this.id = id;
+    this.expectedNotifications = expectedNotifications;
+    this.kuzzle = new Kuzzle(
+      new WebSocket(host, { port: port, sslConnection: port === 443 }),
+      { offlineMode: 'auto' }
+    );
+
+    this.room = null;
+
+    this.disconnectCount = 0;
+    this.reconnectCount = 0;
+
+    this.notifications = [];
+    this.notificationsCount = 0;
+
+    this.kuzzle.addListener('networkError', error => {
+      console.error(error);
+    });
+
+    this.kuzzle.addListener('reconnected', () => {
+      this.reconnectCount += 1;
+    });
+
+    this.kuzzle.addListener('disconnected', () => {
+      this.disconnectCount += 1;
+    });
+  }
+
+  connect () {
+    return this.kuzzle.connect();
+  }
+
+  login (strategy, credentials) {
+    return this.kuzzle.auth.login(strategy, credentials);
+  }
+
+  async start () {
+    this.room = await this.kuzzle.realtime.subscribe(
+      'nyc-open-data',
+      'yellow-taxi',
+      {
+        exists: 'licence'
+      },
+      notification => this.notificationsCount += 1
+    );
+  }
+
+  stop () {
+    return this.kuzzle.realtime.unsubscribe(this.room)
+      .then(() => this.kuzzle.disconnect());
+  }
+
+  ok () {
+    return this.notificationsCount === this.expectedNotifications
+  }
+
+  ko () {
+    return !this.ok()
+  }
+
+  report () {
+    if (this.disconnectCount !== 0 || this.reconnectCount !== 0 || this.ko()) {
+      console.log("\n--------------------------------------------------------------------");
+      if (this.ko()) {
+        console.log(`[Client ${this.id}] ${this.notificationsCount} notifications received`);
+      } else {
+        console.log(`[Client ${this.id}] ${this.disconnectCount} disconnected events`);
+        console.log(`[Client ${this.id}] ${this.reconnectCount} reconnect events`);
+      }
+    }
+
+    // fs.writeFileSync(`${this.id}-report.json`, JSON.stringify({
+    //   disconnectCount: this.disconnectCount,
+    //   reconnectCount: this.reconnectCount,
+    //   notificationsCount: this.notifications.length,
+    //   notifications: this.notifications
+    // }));
+  }
+}
+
+module.exports = Client;
---
+++ realtime/index.js
@@ -0,0 +1,75 @@
+const
+  readline = require('readline'),
+  Client = require('./client');
+
+function askQuestion(query) {
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+  });
+
+  return new Promise(resolve => rl.question(query, ans => {
+      rl.close();
+      resolve(ans);
+  }))
+}
+
+const host = 'localhost';
+const port = 7512;
+const clientCount = 300;
+const expectedNotifications = 16500;
+
+let clientId = 0;
+
+const createClient = () => {
+  const client = new Client(clientId, host, port, expectedNotifications);
+  clientId += 1;
+
+  return client.connect()
+    .then(() => {
+      client.start()
+      return client
+    });
+}
+
+
+
+const clients = [];
+
+const run = async () => {
+  const promises = [];
+  for (let i = 0; i < clientCount; ++i) {
+    promises.push(createClient().then(client => clients.push(client)));
+  }
+  await Promise.all(promises);
+  console.log(`${clientCount} clients connected\n`);
+
+  await askQuestion('Press a key to stop clients');
+
+  clients.forEach(client => {
+    client.report();
+    client.stop();
+  })
+
+  let ko = 0;
+  let missing = 0;
+  let missing2 = 0;
+
+  for (const client of clients) {
+    if (client.ko()) {
+      ko += 1;
+      missing += expectedNotifications - client.notificationsCount;
+    }
+    missing2 += expectedNotifications - client.notificationsCount;
+  }
+  console.log(`${ko} client does not receive ${expectedNotifications} notifications`)
+  console.log(`${missing} total notifications are missing`)
+}
+
+run();
+
+// (async () => {
+//   const cli = new Client(1, host, port)
+//   await cli.connect()
+//   await cli.start()
+// })()
